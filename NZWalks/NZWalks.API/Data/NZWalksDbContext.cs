cb96c2cb438964f8aae60041c6be469b852c4e8c
James Oluwatosin Fadeyi <jamesfa@HA-SHEM.com>
2022-12-16 15:14:39 +0100

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        // create a constructor using ctor + Tab
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> option) : base(option)
+        {
+
+        }
+
+        //Create properties of DbSet types (Each Dbset types is for each class in our Data Model)
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
